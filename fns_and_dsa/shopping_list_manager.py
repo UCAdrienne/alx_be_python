8b4773294b39c2a55743986d6d63d2584a5e31b9
Mbonu Uche <dbmbonu@gmail.com>
2025-08-08 16:52:39 +0100

    Fix: automated test expect an exact prompt string

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item you wish to add")
+            item = input("Enter the item to add")
             shopping_list.append(item)
             print(item, "added to list.")
         elif choice == '2':
